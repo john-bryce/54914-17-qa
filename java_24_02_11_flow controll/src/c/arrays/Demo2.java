5ceec8dbe2de1e4656058905c2bf0e95e05506cf
Eldar <eldarba@gmail.com>
2024-02-14 08:38:53 +0200

    adding some comments to the code

--- java_24_02_11_flow controll/src/c/arrays/Demo2.java
+++ java_24_02_11_flow controll/src/c/arrays/Demo2.java
@@ -7,14 +7,16 @@ public class Demo2 {
         int[] arr = new int[100]; // create an array of length 100
         Random r = new Random(); // an object for creating random numbers
 
-        // insert numbers to the array
+        // insert numbers to the array using for loop
         for(int i = 0; i < arr.length; i++){
             arr[i] = r.nextInt(11);
         }
 
-        // print elements
+        // print elements using for loop again
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + ", ");
         }
+
+        // we could do it using one for loop
     }
 }
