3ea1ce6670952e4e8b87f6e303809ade415bb639
eldarba <eldarba@gmail.com>
2024-02-25 20:56:40 +0200

    lesson 5 - errors

--- java_25_02_2024/src/b/errors/Calculator.java
+++ java_25_02_2024/src/b/errors/Calculator.java
@@ -2,9 +2,31 @@ package b.errors;
 
 public class Calculator {
 
-    int divide(int a, int b) {
-        int result = a / b;
-        return result;
+    void m1() {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        // return;
+    }
+
+    String m2() {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        return "we must some string";
+    }
+
+    // how to correct this method
+    // so there is no error if
+    // b is 0
+    int divide(int a, int b) throws Exception {
+        if (b != 0) {
+            int result = a / b;
+            return result;
+        } else {
+            // create an object of type Exception
+            Exception exception = new Exception("you cannot divide by 0");
+            // throw the exception (not return)
+            throw exception;
+        }
     }
 
 }
--- java_25_02_2024/src/b/errors/CalculatorTest.java
+++ java_25_02_2024/src/b/errors/CalculatorTest.java
@@ -4,9 +4,18 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println(calc.divide(10, 5)); // 2
-        System.out.println(calc.divide(100, 5)); // 20
-        System.out.println(calc.divide(20, 5)); // 4
-        System.out.println(calc.divide(30, 5)); // 6
+
+        try {
+            System.out.println(calc.divide(10, 5)); // 2
+            System.out.println(calc.divide(100, 5)); // 20
+
+            System.out.println(calc.divide(100, 0)); // error
+
+            System.out.println(calc.divide(20, 5)); // 4
+            System.out.println(calc.divide(30, 5)); // 6
+        }catch(Exception e){
+            // what to do in case of an exception
+            System.out.println("there was an error");
+        }
     }
 }
