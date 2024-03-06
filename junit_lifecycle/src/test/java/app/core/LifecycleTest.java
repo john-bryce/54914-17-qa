91139ca713054b6b3f39794e60e5d7cd53667fd5
eldarba <eldarba@gmail.com>
2024-03-06 18:56:31 +0200

    aaa

--- junit_lifecycle/src/test/java/app/core/LifecycleTest.java
+++ junit_lifecycle/src/test/java/app/core/LifecycleTest.java
@@ -6,18 +6,20 @@ import org.junit.jupiter.api.*;
 public class LifecycleTest {
 
     @Test
+    @Order(1)
     void firstTest() {
         System.out.println("first test");
     }
 
     @Test
+    @Order(2)
     void secondTest() {
         System.out.println("second test");
     }
 
     @Test
+    @Order(3)
     void thirdTest() {
         System.out.println("third test");
     }
-
 }
