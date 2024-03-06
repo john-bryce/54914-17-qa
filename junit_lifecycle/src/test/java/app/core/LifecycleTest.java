c46e9900b9180f6e86d5a3e70b1e56ececda92de
eldarba <eldarba@gmail.com>
2024-03-06 18:35:06 +0200

    aaa

--- junit_lifecycle/src/test/java/app/core/LifecycleTest.java
+++ junit_lifecycle/src/test/java/app/core/LifecycleTest.java
@@ -1,4 +1,17 @@
 package app.core;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LifecycleTest {
+
+    @Test
+    @Order(1)
+    void firstTest(){
+
+    }
+
 }
