2f8e828a0bda0908162698c511b9ef996f5c7b34
eldarba <eldarba@gmail.com>
2024-03-06 18:51:05 +0200

    aaa

--- junit_lifecycle/src/test/java/app/core/LifecycleTest.java
+++ junit_lifecycle/src/test/java/app/core/LifecycleTest.java
@@ -1,17 +1,23 @@
 package app.core;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LifecycleTest {
 
     @Test
-    @Order(1)
-    void firstTest(){
+    void firstTest() {
+        System.out.println("first test");
+    }
 
+    @Test
+    void secondTest() {
+        System.out.println("second test");
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("third test");
     }
 
 }
