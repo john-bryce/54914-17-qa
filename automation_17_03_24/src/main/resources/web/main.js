0488daab24bb597bbf6556aa607a12b44bbe6764
Eldar <eldarba@gmail.com>
2024-03-17 19:35:56 +0200

    show hide images

--- automation_17_03_24/src/main/resources/web/main.js
+++ automation_17_03_24/src/main/resources/web/main.js
@@ -1,3 +1,13 @@
 function imageDisplay(event) {
-    console.log("hi");
+    let checkbox = event.target; // get the checkbox object
+    // console.dir(checkbox); // print the checkbox object
+    // console.log(checkbox.checked); // print the state of the checkbox (true / false)
+    let divImages = document.getElementById("div-images");
+    if(checkbox.checked){
+        // show the images
+        divImages.style = "";
+    }else{
+        // hide the images
+        divImages.style = "display: none;";
+    }
 }
